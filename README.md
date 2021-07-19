741174f7207aa58324f726a22d2771e331fa2540
James C <james.clague@brct.io>
2021-07-19 23:02:01 +0000

    v0.0.2 - Preliminary command implementation

--- README.md
+++ README.md
@@ -5,12 +5,57 @@ This is a python script for automating the game [Space Traders](https://spacetra
 ## Installation
 
 Install pipenv: `pip install --user pipenv`
+
 Source your profile: `source ~./profile`
+
 Run `launchdev.sh` and pipenv will handle installing dependencies
 
 ## Changelog
+
+### v0.0.2 - Preliminary command implementation
+
+- Read `.starplan` text files and parse out basic commands
+- - `# <comment>` Ignored
+- - `get ...` Get request
+- - - `info` Specifies account info (credits, ship count, etc.)
+- - - `loans` Specifies loan info (all loans associated with account)
+- - - `loc ...` Specify location request
+- - - - `info` Specifies location info (traits, type, name, etc.)
+- - - - `ships` Specifies list of ships docked at location (ship id, owner, type)
+- - - - `market` Specifies market data from location (good names, prices, quantity, etc.)
+- - - `ships` Specifies list of all owned ships
+- - - `ship` Specifies info about single ship
 
 ### v0.0.1
 
 - SpacePyTraders SDK installed, able to do basic query
 - Setup pipenv
+
+## Roadmap
+
+- 1:1 command parity with SDK/API endpoints
+- `.starsession` file for remembering credentials
+- - Auto-register & run startup script (+notify) on server wipe
+- `.starsettings` file for remembering settings (e.g. log verbosity, notification preferences, etc.)
+- `.starlog` file for logging longitudinal data (changes will be streamed to captains-log for filing in DB for long-term storage)
+- `.starhistory` file for logging commands and their source (plan \[and how plan was started/assigned], cli, helmsman, etc.), errors, notable events, etc.
+- Notifications
+- - Will be able to notify user through various avenues when plan (cmd) or script (hard-coded events like wipe or unrecoverable errors) encounters the need to
+- - Integration with Discord as a bot?
+- - Email?
+- - SMS?
+- Commands for working with **Plans** in `/plans`
+- - `list`, `new`, `edit`, `del`, etc.
+- - `loop` and `goto` commands for use within plans
+- - conditionals
+- - Consider more how plans interact with ships, probably most simply by keeping a list of ships locally, and storing an assigned plan for each. OR by storing a list of active plans, and an associated list of assigned ships?
+- Interactive CLI, perhaps using [Nubia](https://github.com/facebookincubator/python-nubia)
+- Consider alternative UI options later (e.g. web/windows)
+- API for integration with helmsman
+- - Primarily for working on plans remotely
+- - Also allows assigning different plans to ships remotely (using plans local to starscript)
+- Integration with captains-log
+- - Can save API calls by passing information directly from starscript to captains-log (if passed relevant data CL can skip next automatic data poll, otherwise will simply ignore the input)
+- space-opera: Allow for ship `groups` and `pools`
+- - `groups`: Ships that are grouped will all use the same plan (maybe include in base starscript)
+- - `pools`: Ships that are part of a pool can be dynamically assigned to certain plans conditionally. Ships can be a part of multiple pools but (maybe?) are only able to be active in one pool at a time
--- starscript.py
+++ starscript.py
@@ -1,8 +1,115 @@
 from SpacePyTraders import client
+import logging
+# logging config
+logger = logging.getLogger()
+#logging.CRITICAL / .WARNING
+logger.setLevel(level=logging.CRITICAL)
+
+# API Setup
 
 USERNAME = "Greenitthe"
 TOKEN = "c8283f54-c08f-4773-8c40-fc99b0071a19"
 
 api = client.Api(USERNAME, TOKEN)
+print("[GAME API STATUS]", api.game.get_game_status()['status'])
 
-print(api.account.info())
+# Plan Helper Functions
+def interpretCommand(line):
+  cmd = line.split()
+  if (cmd[0] == "#"):
+    # print("Comment: ", cmd[1:])
+    pass
+  elif (cmd[0] == "get"):
+    print("[GET] ", cmd[1:])
+    if (cmd[1] == "info"):
+      info = api.account.info()['user']
+      printLevel(1, "User: " + info['username'])
+      printLevel(2, "Credits: " + str(info['credits']))
+      printLevel(2, "Ships: " + str(info['shipCount']))
+      printLevel(2, "Structures: " + str(info['structureCount']))
+    elif (cmd[1] == "loans"):
+      loans = api.loans.get_user_loans()['loans']
+      for loan in loans:
+        printLevel(1, "Loan ID: " + str(loan['id']))
+        printLevel(2, "Type: " + str(loan['type']))
+        printLevel(2, "Status: " + str(loan['status']))
+        printLevel(2, "Repayment Amount: " + str(loan['repaymentAmount']))
+    elif (cmd[1] == "loc"):
+      if (cmd[2] == "info"):
+        try:
+          loc = api.locations.get_location(cmd[3])['location']
+        except Exception as e:
+          print("[WARNING] Not a valid location or could not get info (" + str(cmd[3]) + ").")
+        else:
+          printLevel(1, "Location info for symbol " + str(cmd[3]) + ":")
+          printLevel(2, "Name: " + str(loc['name']))
+          printLevel(2, "Type: " + str(loc['type']))
+          printLevel(2, "Coords: (" + str(loc['x']) + ", " + str(loc['y']) + ")")
+          printLevel(2, "Allows Construction: " + str(loc['allowsConstruction']))
+          printLevel(2, "Traits:")
+          for trait in loc['traits']:
+            printLevel(3, str(trait))
+          printLevel(2, "Docked Ships: " + str(loc['dockedShips']))
+      elif (cmd[2] == "ships"):
+        try:
+          ships = api.locations.get_ships_at_location(cmd[3])['ships']
+        except Exception as e:
+          print("[WARNING] Not a valid location, or no owned ships at location to provide data. Location: (" + str(cmd[3]) + ").")
+        else:
+          numShips = len(ships)
+          if (numShips > 15):
+            printLevel(1, "Too many ships docked, won't print. Number Docked: " + str(len(ships)))
+          else:
+            printLevel(1, "Ships docked at " + str(cmd[3]) + ":")
+            for ship in ships:
+              printLevel(2, "Ship Type: " + str(ship['shipType']))
+              printLevel(3, "Owner: " + str(ship['username']))
+              printLevel(3, "Ship ID: " + str(ship['shipId']))
+      elif (cmd[2] == "market"):
+        try:
+          market = api.locations.get_marketplace(cmd[3])['marketplace']
+        except Exception as e:
+          print("[WARNING] Not a valid location, or no owned ships at location to provide data. Location: (" + str(cmd[3]) + ").")
+        else:
+          printLevel(1, "Marketplace Listings At " + str(cmd[3]))
+          for listing in market:
+            printLevel(2, str(listing['symbol']))
+            printLevel(3, "Sell Price: " + str(listing['sellPricePerUnit']))
+            printLevel(3, "Buy Price: " + str(listing['purchasePricePerUnit']))
+            printLevel(3, "Available: " + str(listing['quantityAvailable']))
+            printLevel(4, "Volume per Unit: " + str(listing['volumePerUnit']) + ", Spread: " + str(listing['spread']) + ", Price per Unit: " + str(listing['pricePerUnit']))
+    elif (cmd[1] == "ships"):
+      ships = api.ships.get_user_ships()['ships']
+      for ship in ships:
+        printShipInfo(ship)
+    elif (cmd[1] == "ship"):
+      try:
+        ship = api.ships.get_ship(cmd[2])['ship']
+      except Exception as e:
+        print("[WARNING] Not a valid ship id, or could not get ship details. ID: (" + str(cmd[2]) + ").")
+      else:
+        printShipInfo(ship)
+
+def printLevel(level, text):
+  res = "".join([" -"]*(level)) + " " + text
+  print(res)
+
+def printShipInfo(ship):
+  printLevel(1, "Ship ID: " + str(ship['id']))
+  printLevel(2, "Manufacturer: " + str(ship['manufacturer']) + " | Type: " + str(ship['type']) + " | Class: " + str(ship['class']))
+  printLevel(2, "Location: " + str(ship['location']) + " | Coordinates: (" + str(ship['x']) + ", " + str(ship['y']) + ")")
+  printLevel(2, "Flight Speed: " + str(ship['speed']) + " | Plating: " + str(ship['plating']) + " | Weapons: " + str(ship['weapons']))
+  printLevel(2, "Cargo (Current " + str(ship['maxCargo'] - ship['spaceAvailable']) + "/" + str(ship['maxCargo']) + " Max):")
+  printLevel(2, "Loading Speed: " + str(ship['loadingSpeed']))
+  for good in ship['cargo']:
+    printLevel(3, "Good: " + str(good['good']))
+    printLevel(4, "Quantity: " + str(good['quantity']) + " | Volume: " + str(good['totalVolume']))
+
+# File Stuff
+planfile = open("plans/test.starplan", "r")
+plan = planfile.readlines()
+planfile.close()
+
+commands = list(map(lambda line: line.strip('\n'), plan))
+for cmd in commands:
+  interpretCommand(cmd)
